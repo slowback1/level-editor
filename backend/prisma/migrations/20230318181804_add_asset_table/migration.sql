549bf0278858a1cb18dc88c524a8dd83e7294269
drew wobeck <dwobeck@gmail.com>
2023-03-18 13:19:54 -0500

    create asset table and migration

---
+++ backend/prisma/migrations/20230318181804_add_asset_table/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Asset" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "data" BYTEA NOT NULL,
+    "itemWidth" INTEGER NOT NULL,
+    "itemHeight" INTEGER NOT NULL,
+    "ItemSpacing" INTEGER NOT NULL,
+    "projectId" INTEGER NOT NULL,
+
+    CONSTRAINT "Asset_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Asset" ADD CONSTRAINT "Asset_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
