a6b1ff417900e7030d1c619316d6301ec2cce454
slowback1 <dwobeck@gmail.com>
2023-04-30 15:43:04 -0500

    some minor style changes

--- frontend/src/ui/SlowbackInput/SlowbackInput.svelte
+++ frontend/src/ui/SlowbackInput/SlowbackInput.svelte
@@ -1,35 +1,36 @@
 <script lang="ts">
-
-    export let label: string = "";
-    export let onChange: (e: any) => void = () => {};
-    export let type: string = "text";
+	export let label: string = '';
+	export let onChange: (e: any) => void = () => {};
+	export let type: string = 'text';
 </script>
 
 <label class="slowback-input__label" part="slowback-input__label">
-    {label}
-    <input type={type} on:change={onChange} part="slowback-input" class="slowback-input" />
+	{label}
+	<input {type} on:change={onChange} part="slowback-input" class="slowback-input" />
 </label>
 
 <style lang="scss">
-    @import "../../style/index.scss";
+	@import '../../style/index.scss';
 
-.slowback-input {
-    border-radius: 5px 5px 0 5px;
-    border: 1px solid $gunmetal;
-    padding: 8px 12px;
-    font-size: 20px;
-    &:focus {
-        outline: 1px solid $burnt-sienna;
-    }
+	.slowback-input {
+		border-radius: 5px 5px 0 5px;
+		border: 1px solid $gunmetal;
+		padding: 8px 12px;
+		font-size: 20px;
+		&:focus {
+			outline: 1px solid $burnt-sienna;
+		}
 
-    &__label {
-        display: flex;
-        flex-direction: column;
-        font-family: 'Almarai';
-        font-size: 16px;
-        color: $gunmetal;
-        font-weight: 700;
-        gap: 4px;
-    }
-}
+		&__label {
+			display: flex;
+			flex-direction: column;
+			justify-content: flex-start;
+			align-items: flex-start;
+			font-family: 'Almarai';
+			font-size: 16px;
+			color: $gunmetal;
+			font-weight: 700;
+			gap: 4px;
+		}
+	}
 </style>
